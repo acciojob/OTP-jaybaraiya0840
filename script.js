15873e56ed21d95a8bbfd2b5c729bc761400b9d0
jaybaraiya0840 <jaybaraiya0802@gmail.com>
2025-02-17 21:44:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+    const otpInputs = document.querySelectorAll(".code");
+
+    otpInputs.forEach((input, index) => {
+        input.addEventListener("input", (event) => {
+            const value = event.target.value;
+            if (value.length === 1 && index < otpInputs.length - 1) {
+                otpInputs[index + 1].focus();
+            }
+        });
+
+        input.addEventListener("keydown", (event) => {
+            if (event.key === "Backspace") {
+                if (input.value === "" && index > 0) {
+                    otpInputs[index - 1].focus();
+                }
+            }
+        });
+    });
+});
